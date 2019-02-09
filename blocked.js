beb6752f29d1c57d83711ab03de1bfa629a18beb
morch028 <morch028@uottawa.ca>
2019-02-09 18:51:40 -0500

    Fixes animations on blocked page interfering with one another

--- blocked.js
+++ blocked.js
@@ -1,6 +1,6 @@
 (function(){
     const queryParams = new URLSearchParams(window.location.search);
-    const blockedUrl = queryParams.get('url');
+    const blockedUrl = queryParams.get('url') || "today";
 
     const urlField = document.querySelector(".url-field");
     urlField.innerHTML = blockedUrl;
